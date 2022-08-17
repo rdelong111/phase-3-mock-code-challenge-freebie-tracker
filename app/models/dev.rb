14c5f7ce72dfae0a32fb36e543cbc3718ed3671a
Ryan DeLong <rdelong111@gmail.com>
2022-08-16 23:32:08 -0500

    commit tree f93a7461f5bef2963464f42e4c6abe39fa0482b0

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    return true unless self.freebies.where("item_name = ?", item_name).length == 0
+    false
+  end
+
+  def give_away(dev, freebie)
+    if self.id == freebie.dev_id
+      freebie.dev_id = dev.id
+      freebie
+    else
+      "This is not yours to give away."
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dundler_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+Freebie.create(item_name: "Sweatshirt", value: 25, company_id: google.id, dev_id: gazorpazop.id)
+Freebie.create(item_name: "Shirt", value: 10, company_id: facebook.id, dev_id: mr_meseeks.id)
+Freebie.create(item_name: "USB Drive", value: 15, company_id: dundler_mifflin.id, dev_id: morty.id)
+Freebie.create(item_name: "Keychain", value: 5, company_id: enron.id, dev_id: rick.id)
+Freebie.create(item_name: "Backpack", value: 30, company_id: google.id, dev_id: rick.id)
+Freebie.create(item_name: "Bookmark", value: 1, company_id: facebook.id, dev_id: rick.id)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
